6799dbc9a64677bde36a3adfcb57c7e73c1452d9
eclipse_ci <eclipse_ci@qq.com>
2018-12-23 14:32:28 +0800

    44

--- src/main/java/tangjiang/test/ci/App.java
+++ src/main/java/tangjiang/test/ci/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!111" );
+        System.out.println( "Hello World!" );
+        System.out.println(System.getenv("p1"));
     }
 }
